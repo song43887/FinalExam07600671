59321b8ca71d529224bca23cc4d66615429f9bf9
songaiya <teeopen6000@hotmail.com>
2019-11-17 12:19:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/finalexam07600671/model/Auth.java
@@ -0,0 +1,22 @@
+package com.example.finalexam07600671.model;
+
+public class Auth {
+   /* private static final String USER = "aaa";
+    private static final String USER2 = "bbb";
+    private static final String PASSWORD = "111";
+    private static final String PASSWORD2 = "222";
+    private String mUSER;
+    private String mPassword;
+
+    public Auth(String user, String password) {
+        this.mUSER = user;
+        this.mPassword = password;
+    }
+    public boolean check() {
+        if (mUSER.equals(USER) || mUSER.equals(USER2) && mPassword.equals(PASSWORD) || mPassword.equals(PASSWORD2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }*/
+}
